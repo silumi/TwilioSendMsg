93ce7bbd1113e9b4c0cbf98960fb9d4bad0ff41f
silumi <silumikaundika@gmail.com>
2019-12-29 17:53:45 +0530

    sent messages using Twilio and .net

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace TwilioSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        const string accountSid = "AC24be2c58473a315664de0e7518fb8e47";
+        const string authToken = "083dbb87783b4fb45633e8d33497048b";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = MessageResource.Create(
+            body: "Love my self :) I do !",
+            from: new Twilio.Types.PhoneNumber("+18642147841"),
+            to: new Twilio.Types.PhoneNumber("+94714774883")
+        );
+
+        Console.WriteLine(message.Sid);
+
+        }
+    }
+}
